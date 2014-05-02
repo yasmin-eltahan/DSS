5140f00edaa6b5a660c9c9608963789cda662d06
yasmin eltahan <yasmin.eltahan@gmail.com>
2014-05-02 16:04:03 +0200

    migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ end
 def after_sign_in_path_for(resource)
   session[:previous_url] || root_path
 end
+
+def after_sign_out_path_for(resource)
+  session[:previous_url] || root_path
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,25 @@ end
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me , :username , :login
   # attr_accessible :title, :body
+  def login=(login)
+    @login = login
+  end
 
+  def login
+    @login || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
